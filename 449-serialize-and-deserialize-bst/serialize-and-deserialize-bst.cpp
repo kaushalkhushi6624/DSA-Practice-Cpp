b7088ce2e403d31fc2fa2aa498eaeec4828d2e5c
khushi kaushal <kaushalkhushi6624@gmail.com>
2026-09-01 00:18:03 +0530

    Time: 25 ms (41.46%) | Memory: 28 MB (87.78%) - LeetSync

---
+++ 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (!root) return "null";
+        
+        string result;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            
+            if (node) {
+                result += to_string(node->val) + ",";
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                result += "null,";
+            }
+        }
+        return result;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if (data == "null") return nullptr;
+        
+        stringstream ss(data);
+        string val;
+        getline(ss, val, ',');
+        
+        TreeNode* root = new TreeNode(stoi(val));
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            
+            // Left child
+            if (getline(ss, val, ',')) {
+                if (val != "null") {
+                    node->left = new TreeNode(stoi(val));
+                    q.push(node->left);
+                }
+            }
+            
+            // Right child
+            if (getline(ss, val, ',')) {
+                if (val != "null") {
+                    node->right = new TreeNode(stoi(val));
+                    q.push(node->right);
+                }
+            }
+        }
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
