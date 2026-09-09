0800a064a7e0339cd50ab4b2fb8ae736d762ba71
khushi kaushal <kaushalkhushi6624@gmail.com>
2026-09-09 22:02:42 +0530

    Time: 5 ms (32.60%) | Memory: 38.8 MB (33.03%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_set<int> st;
+
+    bool dfs(TreeNode* root, int k) {
+        if (!root) return false;
+
+        if (st.count(k - root->val))
+            return true;
+
+        st.insert(root->val);
+
+        return dfs(root->left, k) || dfs(root->right, k);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        return dfs(root, k);
+    }
+};
